ca493c63c7e3a97e67cd659e3bfbfa86f2fe31ad
SeanM96 <sean-9708@hotmail.co.uk>
2019-04-17 23:22:04 +0100

    refs: more react shizzle

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -2,11 +2,12 @@ package com.example.demo.controllers;
 
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 public class IndexController {
 
     private final PersonService personService;
@@ -14,5 +15,10 @@ public class IndexController {
     @Autowired
     public IndexController(PersonService personService) {
         this.personService = personService;
+    }
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello, the time at the server is now " + new Date() + "\n";
     }
 }
